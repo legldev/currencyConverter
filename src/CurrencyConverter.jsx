74c6d5ef227a9a2de825f02bd003bf92989ab569
Luis Garces <luisgarcesleon@gmail.com>
2020-11-17 00:20:31 -0300

    fixes

--- src/CurrencyConverter.jsx
+++ src/CurrencyConverter.jsx
@@ -88,40 +88,45 @@ const CurrencyConverter = () => {
   };
 
   return (
-      <form class="grid-container">
-          <div class="Labels">
-            <label for="currency-source">Source symbol</label>
-            <label for="currency-destination">Destination symbol</label>
-            <label for="currency-date">Date</label>
-          </div>
-          <div class="Inputs">
-            <input
-              className="currency-source"
-              placeholder="USD"
-              name="currency-source"
-              type="text"
-              value={source}
-              onChange={(e) => currencySource(e)}
-            />
-            <input
-              className="currency-destination"
-              placeholder="EUR"
-              name="currency-destination"
-              value={destination}
-              type="text"
-              onChange={currencyDestination}
-            />
-            <input
-              type="text"
-              placeholder="YYYY-MM-DD"
-              name="currency-date"
-              value={date}
-              className="currency-date"
-              onChange={rateDate}
-            />
-          </div>
+    <form class="container mt-5">
+      <div class="row mt-5">
+        <div class="col-md-3"></div>
+        <div class="col-md-3 d-grid">
+          <label for="currency-source">Source symbol</label>
+          <label for="currency-destination">Destination symbol</label>
+          <label for="currency-date">Date</label>
+        </div>
+        <div class="col-md-3">
+          <input
+            className="currency-source"
+            placeholder="USD"
+            name="currency-source"
+            type="text"
+            value={source}
+            onChange={(e) => currencySource(e)}
+          />
+          <input
+            className="currency-destination"
+            placeholder="EUR"
+            name="currency-destination"
+            value={destination}
+            type="text"
+            onChange={currencyDestination}
+          />
+          <input
+            type="text"
+            placeholder="YYYY-MM-DD"
+            name="currency-date"
+            value={date}
+            className="currency-date"
+            onChange={rateDate}
+          />
+        </div>
+        <div class="col-md-3"></div>
+      </div>
 
-        <div class="Buttons">
+      <div class="row text-center m-5">
+        <div class="col-md-12 p-3">
           <button
             onClick={handleSubmit}
             type="submit"
@@ -139,13 +144,17 @@ const CurrencyConverter = () => {
             reset
           </button>
         </div>
-        <div class="Results">
-        <div className="conversion-result">
-          <p>{result}</p>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-12"> 
+          <div class="Results">
+            <div className="conversion-result">
+              <p>Result: {result}</p>
+            </div>
+          </div>
         </div>
-        </div>
-        
-      </form>
+      </div>
+    </form>
   );
 };
 
